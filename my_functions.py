73a3a07734f9786d409c21b5690460fad4cb9fb2
shayyo <shay.yosef@gmail.com>
2021-07-22 18:53:51 +0300

    second commit2

--- my_functions.py
+++ my_functions.py
@@ -2,3 +2,7 @@
 
 def add(x, y):
     return x + y
+
+
+def multiplication(x, y):
+    return x * y
--- test_my_functions.py
+++ test_my_functions.py
@@ -1,5 +1,10 @@
-from my_functions import add
+from my_functions import add, multiplication
 
 
 def test_add():
     assert add(4, 77) == 81
+
+
+def test_multiplication():
+    assert multiplication(9, 9) == 81
+
