3a159b9c37e586bb7390b3e94065be835b63e990
shayyo <shay.yosef@gmail.com>
2021-08-26 20:50:25 +0300

    ddd

--- my_fixtures.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-@pytest.fixture
-def import_fixture():
-    age = "ddd"
-    return age
--- my_functions.py
+++
@@ -1,8 +0,0 @@
-
-
-def add(x, y):
-    return x + y
-
-
-def multiplication(x, y):
-    return x * y
